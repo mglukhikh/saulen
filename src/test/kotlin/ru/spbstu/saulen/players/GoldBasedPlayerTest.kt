3d1e2af803096e6d42db56acf40c6887bc743bee
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2018-05-09 00:14:17 +0300

    Gold-based player: test with three opponents added

--- src/test/kotlin/ru/spbstu/saulen/players/GoldBasedPlayerTest.kt
+++ src/test/kotlin/ru/spbstu/saulen/players/GoldBasedPlayerTest.kt
@@ -15,4 +15,20 @@ class GoldBasedPlayerTest {
         val results = controller.runGame()
         assertTrue(results[p1]!! > results[p2]!!)
     }
+
+    @Test
+    fun contestWithThreeSimplePlayers() {
+        val p1 = GoldBasedPlayer(0)
+        val p2 = SimpleTestPlayer(1)
+        p2.randomMasters = true
+        val p3 = SimpleTestPlayer(2)
+        val p4 = SimpleTestPlayer(3)
+        p4.randomMasters = true
+        val controller = Controller(p1, p2, p3, p4)
+        controller.silent = false
+        val results = controller.runGame()
+        assertTrue(results[p1]!! > results[p2]!!)
+        assertTrue(results[p1]!! > results[p3]!!)
+        assertTrue(results[p1]!! > results[p4]!!)
+    }
 }
