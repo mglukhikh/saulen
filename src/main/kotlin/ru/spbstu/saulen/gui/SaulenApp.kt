801621d7613a65e2bafe4cf5ee135751d890949b
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2018-11-10 11:29:27 +0300

    Make main function a bit cleaner

--- src/main/kotlin/ru/spbstu/saulen/gui/SaulenApp.kt
+++ src/main/kotlin/ru/spbstu/saulen/gui/SaulenApp.kt
@@ -1,10 +1,10 @@
 package ru.spbstu.saulen.gui
 
-import javafx.application.Application
 import tornadofx.App
+import tornadofx.launch
 
 class SaulenApp : App(SaulenView::class)
 
 fun main(args: Array<String>) {
-    Application.launch(SaulenApp::class.java, *args)
+    launch<SaulenApp>(args)
 }
