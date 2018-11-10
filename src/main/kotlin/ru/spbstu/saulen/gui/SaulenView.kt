2c3f90b92656da04b0b4a05f8927470bacfea806
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2018-11-10 12:36:26 +0300

    GUI: stub for main layout and controller

--- src/main/kotlin/ru/spbstu/saulen/gui/SaulenView.kt
+++ src/main/kotlin/ru/spbstu/saulen/gui/SaulenView.kt
@@ -1,8 +1,31 @@
 package ru.spbstu.saulen.gui
 
 import javafx.scene.layout.BorderPane
-import tornadofx.View
+import tornadofx.*
+import ru.spbstu.saulen.players.Controller as SaulenController
 
 class SaulenView : View() {
+    private val gameController = SaulenController()
+
     override val root = BorderPane()
+
+    init {
+        with (root) {
+            center {
+
+            }
+            top {
+
+            }
+            bottom {
+
+            }
+            left {
+
+            }
+            right {
+
+            }
+        }
+    }
 }
